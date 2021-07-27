167ab78698f671efa774498c5d4c4d18be1f590c
aperevezenseva <perevezenseva.a@gmail.com>
2021-07-27 08:16:44 +0400

    Change column rendering, expand row

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { get } from "lodash";
 
+export interface IRow {
+  row: string;
+  name: string;
+  expandable: boolean;
+  expanded: boolean;
+  parent: IRow | null;
+  path: string;
+}
 export interface IChild {
   name: string;
   number: number | null;
@@ -12,6 +21,7 @@ export interface IData {
   date: Date | null;
   select: string | null;
   file: FormData | null;
+  [k: string]: any;
 }
 
 
@@ -128,29 +138,93 @@ export class TableComponent implements OnInit {
       select: 'Option 1'
     }
   ];
+  rows: IRow[] = [];
+
+  getRows() {
+    const rows: IRow[] = [];
+    const titles = this.data[0].titles;
+    titles.forEach((title, index) => {
+      const row = {
+        name: title.name,
+        row: 'row-' + index,
+        expandable: title.children.length > 0,
+        expanded: true,
+        parent: null,
+        path: "['titles'][" + index + "]"
+      };
+      rows.push(row);
+      title.children.forEach((title2, index2) => {
+        const row2 = {
+          name: title2.name,
+          row: 'row-' + index + '-' + index2,
+          expandable: title2.children.length > 0,
+          expanded: true,
+          parent: row,
+          path: "['titles'][" + index + "]['children'][" + index2 + "]"
+        };
+        rows.push(row2);
+        title2.children.forEach((title3, index3) => {
+          const row3 = {
+            name: title3.name,
+            row: 'row-' + index2 + '-' + index3,
+            expandable: title3.children.length > 0,
+            expanded: true,
+            parent: row2,
+            path: "['titles'][" + index + "]['children'][" + index2 + "]['children'][" + index3 + "]"
+          };
+          rows.push(row3);
+        });
+      });
+    });
+    return rows;
+  }
+
+  expandRow(row: IRow) {
+    const children: IRow[] = this.rows.filter(r => r.parent && r.parent.row === row.row) || [];
+    if (children.filter((ch) => ch.expandable && ch.expanded).length === 0) {
+      row.expanded = !row.expanded;
+    }
+  }
+
+  getByPath(object: IData, path: string) {
+    return get(object, path);
+  }
 
   downloadFile(data: any) {
-    if(!data) return;
-    const blob = new Blob([data], { type: 'application/pdf' });
-    const url = window.URL.createObjectURL(data); 
+    if (!data) return;
+    const url = window.URL.createObjectURL(data);
     window.open(url);
   }
 
-  onFocusOutEvent(event: any, year: any, field: string) {
-    if (field === 'file') {
-      if (event.target.files && event.target.files.length) {
-          const arrFiles: File[] = Array.from(event.target.files);
-          const formData = new FormData();
-          formData.append("file", arrFiles[0], arrFiles[0].name);
-          year[field] = event.target.files[0];
-      }
-    } else {
-      year[field] = event.target.value;
+  onFocusOutEvent(event: any, year: any, path: string, type: string) {
+    switch (type) {
+      case 'date':
+        year[path] = event.target.value;
+        break;
+      case 'file':
+        this.changeFile(event, year, path);
+        break;
+      case 'select':
+        year[path] = event.target.value;
+        break;
+      case 'number':
+        const _updatedObj = this.getByPath(year, path)
+        _updatedObj.number = event.target.value;
+        this.calculateSum();
+        break;
+      default:
+        year[path] = event.target.value;
+        break;
     }
-    if (field === 'number') {
-      this.calculateSum();
+  }
+
+  changeFile(event: any, year: any, path: string) {
+    if (event.target.files && event.target.files.length) {
+      const arrFiles: File[] = Array.from(event.target.files);
+      const formData = new FormData();
+      formData.append("file", arrFiles[0], arrFiles[0].name);
+      year[path] = event.target.files[0];
     }
-    
   }
 
   calculateSum(): void {
@@ -168,16 +242,13 @@ export class TableComponent implements OnInit {
       this.calculateNode(child);
     });
     const notNull = title.children.filter(c => !!c.number);
-    title.number = notNull && notNull.length ? notNull.map((ch) => Number(ch.number)).reduce((a, b) => a + b, 0): null;
-  }
-
-  expandRow() {
-    
+    title.number = notNull && notNull.length ? notNull.map((ch) => Number(ch.number)).reduce((a, b) => a + b, 0) : null;
   }
 
   constructor() { }
 
   ngOnInit(): void {
+    this.rows = this.getRows();
   }
 
 }
